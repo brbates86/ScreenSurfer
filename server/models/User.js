3734b3312442faa3bd81550a23989b6e10516441
karasu yami <demigodlinty@gmail.com>
2023-02-27 21:09:33 -0600

    added User.js

--- server/models/User.js
+++ server/models/User.js
@@ -7,8 +7,34 @@ const userSchema = new Schema({
         required:true,
         unique: true, 
         trim: true,
-    }
+    },
     email: {
-        
-    }
+       type: String,
+       requiered: true,
+       unique: true,
+       match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+
+    password: {
+        type: String,
+        required: true,
+        match:[A-Za-z],
+        minlength: 5,
+    },
 })
+
+userSchema.pre('save', async function (next) {
+     if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (passowrd) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
