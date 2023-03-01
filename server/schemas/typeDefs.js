d7c741dd8ad8a95b3bab6ab72c47ca91ef9a12f5
Brett Bates <brettbates86@gmail.com>
2023-02-28 20:09:25 -0600

    refactoring

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,6 @@ const typeDefs = gql`
     name: String
     email: String
     password: String
-    skills: [String]!
   }
 
   type Auth {
@@ -17,17 +16,16 @@ const typeDefs = gql`
   type Query {
     profiles: [Profile]!
     profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+   
   }
 
   type Mutation {
     addProfile(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
+    
     removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    
   }
 `;
 
