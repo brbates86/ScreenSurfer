f124a490d00571a97eee1d54e0b41ab354217474
karasu yami <demigodlinty@gmail.com>
2023-02-28 19:15:48 -0600

    added changes and finished movies.js

---
+++ server/models/movies.js
@@ -0,0 +1,25 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bycrypt');
+
+const movieSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true
+        },
+        release:{
+            type: Date
+        },
+        description: {
+            type: String,
+            required: true,
+            
+        },
+        screenTime:{
+            type: String,
+            required:true
+
+        }
+    }
+)
+module.exports = movieSchema
