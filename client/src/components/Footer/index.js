ee005d7647b5d54f108498da24d3a66ef5c6b92d
Brett Bates <brettbates86@gmail.com>
2023-03-01 08:13:46 -0600

    add Footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 
-const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaGithub } from 'react-icons/fa';
+import { ReactNode } from 'react';
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: 
+{
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
   return (
-    <footer >
-      Footer
-    </footer>
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
   );
 };
 
-export default Footer;
+export default function SmallWithSocial() {
+  return (
+    <Box
+      bg={useColorModeValue('blue.500', 'gray.900')}
+      color={useColorModeValue('whiteAlpha.900', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={5}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>© 2022 ScreenSurfer All Rights Reserved</Text>
+        <Stack direction={'row'} spacing={6}>
+          <SocialButton label={'Twitter'} href={'#'}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={'GitHub'} href={'#'}>
+            <FaGithub />
+          </SocialButton>
+          <SocialButton label={'Instagram'} href={'#'}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
+
