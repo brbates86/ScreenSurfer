d9e118e6f468561d7cc9f138465019b2ccf1ac16
Brett Bates <brettbates86@gmail.com>
2023-03-01 08:03:08 -0600

    updates to code

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,44 +1,85 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Flex, Text, Button } from "@chakra-ui/react"
+// import Auth from '../../utils/auth';
+// import Logo from "./public/logo.png";
+// import { CloseIcon, MenuIcon } from ".../Icons"
 
-import Auth from '../../utils/auth';
-
-const Header = () => {
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  };
+const MenuItems = (props) => {
+  const { children, isLast, to = "/", ...rest } = props
   return (
-    <header>
-      <div>
-        <Link>
-         Home
-        </Link>
-        
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link>
-                Profile
-              </Link>
-              <button  onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to='/login'>
-                Login
-              </Link>
-              <Link to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  );
-};
+    <Text
+      mb={{ base: isLast ? 0 : 8, sm: 0 }}
+      mr={{ base: 0, sm: isLast ? 0 : 8 }}
+      display="block"
+      {...rest}
+    >
+      <Link to={to}>{children}</Link>
+    </Text>
+  )
+}
 
-export default Header;
+const Header = (props) => {
+  const [show, setShow] = React.useState(false)
+  const toggleMenu = () => setShow(!show)
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={8}
+      p={8}
+      bg={["whiteAlpha.900", "whiteAlpha.900", "whiteAlpha.900", "blue.500"]}
+      color={["whiteAlpha.700", "whiteAlpha.500", "whiteAlpha.500", "whiteAlpha.900"]}
+      {...props}
+    >
+      <Flex align="center">
+       
+      </Flex>
+
+      <Box display={{ base: "block", md: "none" }} onClick={toggleMenu}>
+       
+      </Box>
+
+      <Box
+        display={{ base: show ? "block" : "none", md: "block" }}
+        flexBasis={{ base: "100%", md: "auto" }}
+      >
+        <Flex
+          align={["center", "center", "center", "center"]}
+          justify={["center", "space-between", "flex-end", "flex-end"]}
+          direction={["column", "row", "row", "row"]}
+          pt={[4, 4, 0, 0]}
+        >
+          <MenuItems to="/">Home</MenuItems>
+          <MenuItems to="/search">Search</MenuItems>
+          <MenuItems to="/profile">Profile</MenuItems>
+          <MenuItems to="/pricing"></MenuItems>
+          <MenuItems to="/signup" isLast>
+            <Button
+              size="sm"
+              rounded="md"
+              color={["red.900", "red.950", "red.400", "whiteAlpha.900"]}
+              bg={["whiteAlpha.500", "whiteAlpha.900", "whiteAlpha.900", "green.300"]}
+              _hover={{
+                bg: [
+                  "red.400",
+                  "primary.500",
+                  "primary.600",
+                  "primary.600",
+                ],
+              }}
+            >
+              Create Account
+            </Button>
+          </MenuItems>
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Header
